953ec15e8c65017c570300a3d64a00e85f7b4e6e
Edu Marques <e.marques180303@gmail.com>
2021-03-02 12:49:07 -0300

    classe login

--- codigos/login.php
+++ codigos/login.php
@@ -6,6 +6,40 @@ public $email;
 public $senha;
 public $validacao;
 
+//set
+public function setEmail($email){
+    $this->email = $email;
+}
+
+public function setSenha($senha){
+    $this->senha = $senha;
+}
+
+public function setValidacao($validacao){
+    $this->validacao = $validacao;
+}
+
+//get
+public function getEmail(){
+    $this->email;
+}
+
+public function getSenha(){
+    $this->senha;
+}
+
+public function getValidacao(){
+    $this->validacao;
+}
+
+//construtor
+public function __construct($email, $senha, $validacao)
+{
+    $this->email = $email;
+    $this->senha = $senha;
+    $this->validacao = $validacao;
+}
+
 }
 
 ?>
