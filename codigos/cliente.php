7cc36462718b569e95246b05acb2e3f164ebaa56
Edu Marques <e.marques180303@gmail.com>
2021-03-02 23:22:12 -0300

    Classe  cliente

--- codigos/cliente.php
+++ codigos/cliente.php
@@ -6,6 +6,8 @@ class Cliente{
     public $apelido;
     public $consulta;
     public $num_consultas;
+    public $valid;
+    public $conf;
     
 //set
 public function setPlano($plano){
@@ -22,6 +24,14 @@ public function setConsulta($consulta){
 
 public function setNum_consultas($num_consultas){
     $this->num_consultas = $num_consultas;
+}
+
+public function setConf($conf){
+    $this->conf = $conf;
+}
+
+public function setValid($valid){
+    $this->valid = $valid;
 }
 
 //get
@@ -41,21 +51,48 @@ public function getNum_consultas(){
     return $this->num_consultas;
 }
 
-public function __construct($plano, $apelido, $num_consultas, $consulta = null)
+public function getConf(){
+    return $this->conf;
+}
+
+public function getValid(){
+    return $this->valid;
+}
+
+public function __construct($plano, $apelido, $num_consultas, $conf, $consulta = null)
 {
     $this->plano = $plano;
     $this->apelido = $apelido;
     $this->consulta = $consulta;
     $this->num_consultas = $num_consultas;
+    $this->conf = $conf;    
 }
 
 public function Exibir(){
     return "Plano: $this->plano <br> Nº Consultas: $this->num_consultas <br> Nome: $this->apelido";
 }
 
+public function Conf($conf){
+    $this->valid = false;
+    if($conf == "sim"){
+        $this->valid = true;
+    }
 }
 
-$objcliente = new Cliente("Saude Plus", "Edu", "0");
+public function Validando($valid){
+    if ($valid){
+        echo "Dados validados";
+    }else{
+        echo "Dados invalidos";
+    }
+}
+
+}
+
+$objcliente = new Cliente("Saude Plus", "Edu", "0", "sim");
 echo $objcliente->Exibir();
+echo "<br>";
+$objcliente->Conf($objcliente->getConf());
+$objcliente->Validando($objcliente->getValid());
 
 ?>
