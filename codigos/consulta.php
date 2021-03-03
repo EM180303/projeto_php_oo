438b8b9b73a16d251862e205489b2ccf18cad46a
Edu Marques <e.marques180303@gmail.com>
2021-03-03 00:03:20 -0300

    consulta

--- codigos/consulta.php
+++ codigos/consulta.php
@@ -1,5 +1,4 @@
 <?php
-
 class Consulta{
 
 protected $data;
@@ -7,6 +6,8 @@ protected $hora;
 protected $motivo;
 protected $medico;
 protected $cliente;
+public $pergunta;
+public $telefone;
     
 //set
 public function setData($data){
@@ -27,6 +28,14 @@ public function setMedico($medico){
 
 public function setCliente($cliente){
     $this->cliente = $cliente;
+}
+
+public function setPergunta($pergunta){
+    $this->pergunta = $pergunta;
+}
+
+public function setTelefone($telefone){
+    $this->telefone = $telefone;
 }
 
 //get
@@ -50,15 +59,43 @@ public function getMedico(){
     return $this->medico;
 }
 
-public function __construct($cliente, $data, $hora, $motivo, $medico)
+public function getPergunta(){
+    return $this->pergunta;
+}
+
+public function getTelefone(){
+    return $this->telefone;
+}
+
+public function __construct($cliente, $data, $hora, $motivo, $medico, $pergunta, $telefone = null)
 {
     $this->cliente = $cliente;
     $this->data = $data;
     $this->hora = $hora;
     $this->motivo = $motivo; 
     $this->medico = $medico;
+    $this->telefone = $telefone;
+    $this->pergunta = $pergunta;
+}
+
+public function Exibir(){
+    return "Nome do paciente: $this->cliente <br> Data: $this->data <br> Hora: $this->hora <br> Motivo: $this->motivo <br> Medico: $this->medico";
+}
+
+public function PacienteNoLocal($pergunta){
+    echo "<br>";
+    echo"O paciente já chegou? ";
+    if($pergunta == "sim"){
+        echo"$pergunta";
+    }else{
+        echo"$pergunta";
+        echo"<br> Ligue para ele: $this->telefone";
+    }
 }
 
 }
+$objconsulta = new Consulta("Edu", "22/08/2021", "14:00", "Dor no joelho", "Dr. Dráuzio", "sim", 81985624197);
+echo $objconsulta->Exibir();
+$objconsulta->PacienteNoLocal($objconsulta->getPergunta());
 
 ?>
