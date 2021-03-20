08fb667238f5e0f909f6a01347dfc2a3f5474a42
Edu Marques <e.marques180303@gmail.com>
2021-03-20 00:36:26 -0300

    ajustando nascimento no cadastro

--- codigos/cadastrar.php
+++ codigos/cadastrar.php
@@ -7,9 +7,9 @@ class Cadastrar{
 public function inserir($conect)
 	{ 
 		$data = date('YmdHis');
-		$datanascimento = $_POST['nascimento'];
+		
 		$stmt = $conect->prepare("INSERT INTO cadastro (hope_ca_nome,hope_ca_email,hope_ca_senha,hope_ca_nascimento,hope_ca_data,hope_ca_funcao,hope_ca_cep,hope_ca_rua,hope_ca_bairro,hope_ca_cidade,hope_ca_uf,hope_ca_complemento,hope_ca_nresi,hope_ca_telefone) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-		$stmt->bind_param("sssddsssssssii",$_POST['name'], $_POST['email'], $_POST['pass'], $datanascimento, $data, $_POST['função'] ,$_POST['cep'], $_POST['rua'], $_POST['bairro'], $_POST['cidade'], $_POST['uf'], $_POST['complemento'], $_POST['numeroresi'], $_POST['telefone']);
+		$stmt->bind_param("ssssdsssssssii",$_POST['name'], $_POST['email'], $_POST['pass'], $_POST['nascimento'], $data, $_POST['função'] ,$_POST['cep'], $_POST['rua'], $_POST['bairro'], $_POST['cidade'], $_POST['uf'], $_POST['complemento'], $_POST['numeroresi'], $_POST['telefone']);
 		$stmt->execute();
 	}
 
